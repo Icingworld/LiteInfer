ccf1eb6a608f665da5209ea9da432e359b599679
Icingworld <984442022@qq.com>
2026-08-27 00:22:06 +0800

    feat(common): 添加了一些类型定义

---
+++ src/core/common/types.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <cstdint>
+
+namespace liteinfer::core::common
+{
+
+using TokenId = std::uint32_t;
+
+using RequestId = std::uint32_t;
+
+using SequenceId = std::uint32_t;
+
+using KVBlockId = std::uint32_t;
+
+using Position = std::uint32_t;
+
+} // namespace liteinfer::core::common
