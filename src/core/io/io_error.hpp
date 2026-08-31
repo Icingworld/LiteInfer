5d4f24a17dec0b4dd4fe79e622bff92fe86dec32
Icingworld <984442022@qq.com>
2026-08-31 11:20:31 +0800

    feat(io): 添加了 io 层错误类型

---
+++ src/core/io/io_error.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <cstdint>
+
+#include "core/common/error.hpp"
+
+namespace liteinfer::core::io
+{
+
+// IO 错误码
+enum class IoErrorCode : std::uint8_t
+{};
+
+// IO 错误类型
+using IoError = common::Error;
+
+} // namespace liteinfer::core::io
+
+namespace liteinfer::core::common
+{
+
+template <>
+struct ErrorTraits<io::IoErrorCode>
+{
+    static constexpr ErrorCategory category = ErrorCategory::Io;
+};
+
+} // namespace liteinfer::core::common
